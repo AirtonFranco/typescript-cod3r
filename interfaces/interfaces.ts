74fe973577039a543d699d366d37c608ef15580a
AIRTON FRANCO DA SILVEIRA <Z215488@corp.clarobr>
2021-11-23 14:45:55 -0300

    interfaces

--- interfaces/interfaces.ts
+++ interfaces/interfaces.ts
@@ -1,19 +1,19 @@
-interface user {
+interface User {
     nome: string //atributo obrigatorio
     idade?: number //atributo opcional
     [prop: string]: any //atributo dinamico, quando vc quer passar algum parametro dinamico e não precisa passar na interface
     saudar(sobrenome: string): void //pode-se usar função na interface
 }
 
-function saudarComOla(pessoa: user) {
+function saudarComOla(pessoa: User) {
     console.log("Ola " + pessoa.nome)
 }
 
-function mudarNome(pessoa: user) {
+function mudarNome(pessoa: User) {
     return pessoa.nome = "Vane"
 }
 
-const pessoa: user = {
+const pessoa: User = {
     nome: 'Airton',
     idade: 32,
     saudar(sobrenome: string) {
@@ -28,3 +28,18 @@ console.log(mudarNome(pessoa))
 //saudarComOla({nome: 'Airton'})
 //saudarComOla({nome: 'Airton', idade: 32, altura: 1.75})
 pessoa.saudar("franco")
+
+//Usando Classes
+class Cliente implements User {
+    nome: string = ""
+    saudar(sobrenome: string) {
+        console.log('Olá meu nome é: ' 
+        + this.nome + ' ' + sobrenome)
+    }
+}
+
+const meuCliente = new Cliente()
+
+meuCliente.nome = "Han"
+saudarComOla(meuCliente)
+meuCliente.saudar("solo")
